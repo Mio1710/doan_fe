b835adf42312af5d1984415dfd0b0642cf4a58c6
DESKTOP-1A74MO3\admin <nguyenquangtu2301@gmail.com>
2024-10-26 13:22:31 +0700

    import student intern

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,7 @@ import type { AxiosInstance } from 'axios'
 import { SemesterAPI } from '~/api/semester'
 import { TeacherApi } from '~/api/teacher'
 import { StudentTopicAPI } from '~/api/student-topic'
+import { StudentInternAPI } from '~/api/student-intern'
 import { TopicAPI } from '~/api/topic'
 import { FacultyAPI } from '~/api/faculty'
 import { SuperTeacherAPI } from '~/api/super-teacher'
@@ -11,6 +12,7 @@ export class Api {
   public readonly semester: SemesterAPI
   public readonly teacher: TeacherApi
   public readonly studentTopic: StudentTopicAPI
+  public readonly studentIntern: StudentInternAPI
   public readonly topic: TopicAPI
   public readonly faculty: FacultyAPI
   public readonly superTeacher: SuperTeacherAPI
@@ -20,6 +22,7 @@ export class Api {
     this.semester = new SemesterAPI(axios)
     this.teacher = new TeacherApi(axios)
     this.studentTopic = new StudentTopicAPI(axios)
+    this.studentIntern = new StudentInternAPI(axios)
     this.topic = new TopicAPI(axios)
     this.faculty = new FacultyAPI(axios)
     this.superTeacher = new SuperTeacherAPI(axios)
---
+++ composables/teacher/use-teacher-get-student-intern.ts
@@ -0,0 +1,57 @@
+import { computed } from 'vue'
+import { UnwrapRef } from 'vue-demi'
+import { useQuery } from 'vue-query'
+import { map, zip } from 'lodash'
+import Parser from '~/utils/parser'
+function sortsParser(sortBy: string | [], sortType: string | []) {
+  if (!sortBy) {
+    return []
+  }
+
+  if (Array.isArray(sortBy)) {
+    return map(zip(sortBy, sortType), ([sortBy, sortType]) => {
+      return sortType === 'desc' ? `-${sortBy}` : sortBy
+    })
+  }
+
+  return [sortType === 'desc' ? `-${sortBy}` : sortBy]
+}
+
+export default function useTeacherGetStudentInterns(params?: UnwrapRef<any>, options?: any) {
+  const { $api } = useNuxtApp()
+
+  const query = useQuery(
+    ['student-intern', params],
+    () => {
+      const { sortBy, sortType } = params.value
+      const sorts = sortsParser(sortBy, sortType)
+
+      const query = new Parser({
+        includes: [],
+        appends: [],
+        fields: {},
+        filters: {},
+        sorts,
+        page: params.value.page,
+        limit: params.value.rowsPerPage,
+        payload: null,
+        ...params.value,
+      }).query()
+
+      return $api.teacher.getStudentInterns(query)
+    },
+    {
+      refetchOnWindowFocus: false,
+      ...options,
+    },
+  )
+
+  const items = computed(() => query.data.value?.data || [])
+  const totalItems = computed(() => query.data.value?.pagination?.total || 0)
+
+  return {
+    ...query,
+    items,
+    totalItems,
+  }
+}
