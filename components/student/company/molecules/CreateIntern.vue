2ff1181ef5baa2796d6a6351ccc8f8de6069bb0e
Le Nguyen <nguyenthile.work@gmail.com>
2024-12-06 23:33:27 +0700

    [Update] Add label for app text field

--- components/student/company/molecules/CreateIntern.vue
+++ components/student/company/molecules/CreateIntern.vue
@@ -4,20 +4,20 @@ import FormCard from '~/components/common/molecules/FormCard.vue'
 import AppTextField from '~/components/common/atoms/AppTextField.vue'
 
 const form = reactive({
-    company_name: '',
-    address: '',
-    company_phone: '',
-    company_email: '',
-    supervisor_name: '',
-    supervisor_phone: '',
-    supervisor_email: '',
+  company_name: '',
+  address: '',
+  company_phone: '',
+  company_email: '',
+  supervisor_name: '',
+  supervisor_phone: '',
+  supervisor_email: '',
 })
 
 const { $api, $toast } = useNuxtApp()
 const emit = defineEmits(['cancel'])
 const queryClient = useQueryClient()
 
-const createStudentIntern= () => {
+const createStudentIntern = () => {
   $api.intern.createIntern(form).then(() => {
     $toast.success('Đăng ký thực tập thành công')
     queryClient.invalidateQueries('intern')
@@ -31,13 +31,23 @@ const preview = () => {
 </script>
 
 <template>
-  <form-card can-cancel cancel-text="Hủy" title="Đăng ký thưc tập" @cancel="emit('cancel')" @submit="createStudentIntern">
-    <app-text-field v-model="form.company_name" name="Tên công ty" rules="required" />
-    <app-text-field v-model="form.company_email" name="Emai công ty" type="textarea" />
-    <app-text-field v-model="form.company_phone" name="SĐT công ty" />
-    <app-text-field v-model="form.address" name= "Địa chỉ công ty" />
-    <app-text-field v-model="form.supervisor_name" name="Tên người hướng dẫn" />
-    <app-text-field v-model="form.supervisor_phone" name="Số điện thoại người hướng dẫn" />
-    <app-text-field v-model="form.supervisor_email" name="Email người hướng dẫn" />
+  <form-card
+    can-cancel
+    cancel-text="Hủy"
+    title="Đăng ký thưc tập"
+    @cancel="emit('cancel')"
+    @submit="createStudentIntern"
+  >
+    <app-text-field v-model="form.company_name" label="Tên công ty" name="Tên công ty" rules="required" />
+    <app-text-field v-model="form.company_email" label="Emai công ty" name="Emai công ty" type="textarea" />
+    <app-text-field v-model="form.company_phone" label="SĐT công ty" name="SĐT công ty" />
+    <app-text-field v-model="form.address" label="Địa chỉ công ty" name="Địa chỉ công ty" />
+    <app-text-field v-model="form.supervisor_name" label="Tên người hướng dẫn" name="Tên người hướng dẫn" />
+    <app-text-field
+      v-model="form.supervisor_phone"
+      label="Số điện thoại người hướng dẫn"
+      name="Số điện thoại người hướng dẫn"
+    />
+    <app-text-field v-model="form.supervisor_email" label="Email người hướng dẫn" name="Email người hướng dẫn" />
   </form-card>
 </template>
--- components/student/company/molecules/UpdateIntern.vue
+++ components/student/company/molecules/UpdateIntern.vue
@@ -27,14 +27,23 @@ const importStudentIntern = () => {
 </script>
 
 <template>
-    <form-card can-cancel cancel-text="Hủy" title="Đăng ký thưc tập" @cancel="emit('cancel')" @submit="createStudentIntern">
-      <app-text-field v-model="form.company_name" name="Tên công ty" rules="required" />
-      <app-text-field v-model="form.company_email" name="Emai công ty" type="textarea" />
-      <app-text-field v-model="form.company_phone" name="SĐT công ty" />
-      <app-text-field v-model="form.address" name= "Địa chỉ công ty" />
-      <app-text-field v-model="form.supervisor_name" name="Tên người hướng dẫn" />
-      <app-text-field v-model="form.supervisor_phone" name="Số điện thoại người hướng dẫn" />
-      <app-text-field v-model="form.supervisor_email" name="Email người hướng dẫn" />
-    </form-card>
-  </template>
-  
+  <form-card
+    can-cancel
+    cancel-text="Hủy"
+    title="Đăng ký thưc tập"
+    @cancel="emit('cancel')"
+    @submit="createStudentIntern"
+  >
+    <app-text-field v-model="form.company_name" label="Tên công ty" name="Tên công ty" rules="required" />
+    <app-text-field v-model="form.company_email" label="Emai công ty" name="Emai công ty" type="textarea" />
+    <app-text-field v-model="form.company_phone" label="SĐT công ty" name="SĐT công ty" />
+    <app-text-field v-model="form.address" label="Địa chỉ công ty" name="Địa chỉ công ty" />
+    <app-text-field v-model="form.supervisor_name" label="Tên người hướng dẫn" name="Tên người hướng dẫn" />
+    <app-text-field
+      v-model="form.supervisor_phone"
+      label="Số điện thoại người hướng dẫn"
+      name="Số điện thoại người hướng dẫn"
+    />
+    <app-text-field v-model="form.supervisor_email" label="Email người hướng dẫn" name="Email người hướng dẫn" />
+  </form-card>
+</template>
