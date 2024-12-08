7b2ae230729954148553bebec640baacc04c1c5b
DESKTOP-1A74MO3\admin <nguyenquangtu2301@gmail.com>
2024-12-08 14:11:19 +0700

    Update CURD page student-intern

--- pages/admin/faculty/student-company.vue
+++ pages/admin/faculty/student-company.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { useQueryClient } from 'vue-query'
 import { format } from 'date-fns'
 import AppTextField from '~/components/common/atoms/AppTextField.vue'
@@ -90,14 +90,137 @@ const { items, totalItems, isLoading, refetch } = useGetStudentIntern(queryBuild
           <template #item.index="{ index }">
             <span>{{ index + 1 }}</span>
           </template>
-
-          <!-- <template #item.gv="{ item }">
-            <span>
-              {{ item.studentIntern[0]?.intern.teacher.hodem }} {{ item.studentIntern[0]?.topic.teacher.ten }}
-            </span>
-          </template> -->
           <template #item.ten="{ item }">
             <span>{{ item.hodem + ' ' + item.ten }}</span>
+          </template>
+        </v-data-table>
+      </div>
+    </v-card>
+  </div>
+</template>
+
+<style scoped></style> -->
+
+<script setup lang="ts">
+import { format } from 'date-fns'
+import AppTextField from '~/components/common/atoms/AppTextField.vue'
+import ImportStudentIntern from '~/components/admin/student-intern/molecules/ImportStudentIntern.vue'
+import CreateStudentIntern from '~/components/admin/student-intern/molecules/CreateStudentIntern.vue'
+import DeleteTeacherConfirmDialog from '~/components/admin/teacher/molecules/DeleteTeacherConfirmDialog.vue'
+import UpdateStudentIntern from '~/components/admin/student-intern/molecules/UpdateStudentIntern.vue'
+import DeleteStudentInternConfirmDialog from '~/components/admin/student-intern/molecules/DeleteStudentInternConfirmDialog.vue'
+
+definePageMeta({
+  layout: 'auth',
+  middleware: ['is-admin'],
+})
+
+const headers = [
+  {
+    title: 'STT',
+    align: 'center',
+    sortable: false,
+    key: 'index',
+    width: 50,
+  },
+  { title: 'Tên sinh viên', key: 'ten' },
+  { title: 'Mã số', key: 'maso' },
+  { title: 'Email', key: 'email' },
+  { title: 'Lớp', key: 'lop', width: '15%', minWidth: 150 },
+
+  { title: '', key: 'action', width: 100 },
+]
+
+const filters = ref({
+  q: '',
+})
+const serverOptions = ref({
+  page: 1,
+  rowsPerPage: 100,
+  sortBy: '-created_at',
+  sortType: 'asc',
+})
+const queryBuilder = computed(() => ({
+  ...serverOptions.value,
+}))
+
+const { $api, $toast } = useNuxtApp()
+
+const { items, totalItems, isLoading, refetch } = useGetStudentIntern(queryBuilder)
+</script>
+
+<template>
+  <div class="d-flex flex-column flex-grow-1 h-full">
+    <div class="text-lg font-bold text-uppercase">Quản lý sinh viên thực tập</div>
+    <v-card class="px-3 py-2 h-full" color="white" variant="flat">
+      <div class="d-flex items-center">
+        <v-dialog min-width="600" width="60%">
+          <template #activator="{ props: activatorProps }">
+            <v-btn color="success" size="small" v-bind="activatorProps">
+              <v-icon>mdi-plus</v-icon>
+              <span>Import</span>
+            </v-btn>
+          </template>
+          <template #default="{ isActive }">
+            <import-student-intern @cancel="isActive.value = false" />
+          </template>
+        </v-dialog>
+        <app-text-field
+          v-model="filters.q"
+          class="h-[24px] w-[250px] ml-2"
+          clearable
+          density="compact"
+          placeholder="Tên/Mã số sinh viên"
+          prepend-inner-icon="mdi-magnify"
+          variant="plain"
+        />
+        <v-spacer />
+        <v-dialog min-width="400" width="40%">
+          <template #activator="{ props: activatorProps }">
+            <v-btn color="success" size="small" v-bind="activatorProps">
+              <v-icon>mdi-plus</v-icon>
+              <span>Thêm sinh viên</span>
+            </v-btn>
+          </template>
+          <template #default="{ isActive }">
+            <create-student-intern @cancel="isActive.value = false" @success="refetch" />
+          </template>
+        </v-dialog>
+        <v-btn icon size="x-small" variant="text" @click="refetch()">
+          <v-icon>mdi-refresh</v-icon>
+        </v-btn>
+      </div>
+      <div class="mt-2 h-[calc(100%_-_45px)]">
+        <v-data-table
+          class="h-full"
+          fixed-header
+          :headers="headers"
+          hide-default-footer
+          :items="items"
+          :items-per-page="1000"
+          :loading="isLoading"
+        >
+          <template #item.index="{ index }">
+            <span>{{ index + 1 }}</span>
+          </template>
+          <template #item.ten="{ item }">
+            <span>{{ item.hodem + ' ' + item.ten }}</span>
+          </template>
+          <template #item.ngay_sinh="{ item }">
+            <span>{{ format(new Date(item.ngay_sinh), 'dd/MM/yyyy') }}</span>
+          </template>
+          <template #item.action="{ item }">
+            <v-dialog min-width="400" width="40%">
+              <template #activator="{ props: activatorProps }">
+                <v-btn v-bind="activatorProps" color="success" icon size="small" variant="text">
+                  <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+              </template>
+              <template #default="{ isActive }">
+                <update-student-intern :student-intern="item" @cancel="isActive.value = false" />
+              </template>
+            </v-dialog>
+            <delete-student-intern-confirm-dialog :student-intern="item" @success="refetch" />
           </template>
         </v-data-table>
       </div>
